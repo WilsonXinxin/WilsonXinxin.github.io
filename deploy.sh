009ea0ff452db66516077d099bcbe2ea26a3fcbc
wilson <Yinxinxin@UNIVERSEACTION.com>
2022-12-02 11:39:23 +0800

    publish gh-pages

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 #!/usr/bin/env sh
 
+if [ -z "$1" ]
+then
+  echo "Which folder do you want to deploy to GitHub Pages?"
+  exit 1
+fi
+
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -9,22 +15,19 @@ set -e
 yarn build
 
 # 进入生成的文件夹
-cd dist
+# cd dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-git init 
-git add -A
-git commit -m 'deploy'
+# git init 
+# git add -A
+# git commit -m 'deploy'
 
 # https://gist.github.com/cobyism/4730490
-if [ -z "$1" ]
-then
-  echo "Which folder do you want to deploy to GitHub Pages?"
-  exit 1
-fi
 # 发布到子分支 
+git add .
+git commit -m'publish gh-pages'
 git subtree push --prefix $1 origin gh-pages
 
 # 如果发布到 https://<USERNAME>.github.io  USERNAME=你的用户名 
