24780fab45de6dbc106fe83bd4c774bb2bd2df7a
wilson <Yinxinxin@UNIVERSEACTION.com>
2023-06-26 11:19:35 +0800

    tes

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # yarn run publish
 
 #!/usr/bin/env sh
-
+echo
 if [ -z "$1" ]
 then
   echo "Which folder do you want to deploy to GitHub Pages?"
@@ -18,4 +18,5 @@ yarn build
 
 # https://gist.github.com/cobyism/4730490
 # 发布到子分支 
-git subtree push --prefix $1 origin gh-pages
+# git subtree push --prefix $1 origin gh-pages
+git push origin :gh-pages && git subtree push --prefix dist origin gh-pages
